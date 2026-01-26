6f97e576dc7f65d29545c8e0f9f1ff4467185309
Hasan Efe Uygun <hasanefeuygun@Hasan-MBP.home>
2026-01-27 00:05:58 +0300

    getNumbers function added and controllers updated

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('numbers')
+  getNumbers(): number {
+    return this.appService.getNumbers();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getNumbers(): number {
+    return Math.floor(Math.random() * 10 + 1); // Numbers 1 to 10 both included
+  }
 }
