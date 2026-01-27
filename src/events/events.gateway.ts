c0b06e9af83f417b55d0d2cfc77e8eabc13ae517
Hasan Efe Uygun <hasanefeuygun@Hasan-MBP.home>
2026-01-27 11:02:32 +0300

    WebSocketGateway created and events.module added

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,40 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import 'dotenv/config';
+
+interface PingPayload {
+  msg: string;
+}
+
+@WebSocketGateway({
+  cors: {
+    origin: [`http://localhost:${process.env.FRONTEND_PORT}`], // Ui url
+    credentials: true,
+  },
+  namespace: '/realtime', // For Example: client needs to connect http://localhost:3000/realtime
+})
+export class EventsGateway {
+  // İt's similar to controller but this is for socket events
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('connected:', client.id);
+    client.emit('server:hello', { msg: 'connected' });
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log('disconnected:', client.id);
+  }
+
+  @SubscribeMessage('client:ping')
+  onPing(@MessageBody() body: PingPayload, @ConnectedSocket() client: Socket) {
+    client.emit('server:pong', { recieved: body, at: Date.now() });
+  }
+}
